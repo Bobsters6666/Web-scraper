7105f5bcb1720209c08b1e0d1f63ee65c14b6fef
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-10-09 17:06:31 +1300

    fixed build errors -4

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,6 @@ import { connectToDB } from "@/lib/scraper/mongoose"
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
 
